f6cc69a8463963181fb492c325f30fdeebfe1526
Jeffrey Yong <jeffreyyong10@gmail.com>
2018-03-21 23:41:19 +0000

    Simple IP parsing

---
+++ socket/ip/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+	fmt.Printf("name: %v\n", name)
+
+	addr := net.ParseIP(name)
+	fmt.Printf("addr: %v\n", addr)
+	if addr == nil {
+		fmt.Println("Invalid address")
+	} else {
+		fmt.Println("The address is ", addr.String())
+	}
+	os.Exit(0)
+}
